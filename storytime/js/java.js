72314cd381a228c13dbdc63cf1ad4c26f31daf88
gordc262 <35770587+gordc262@users.noreply.github.com>
2018-02-21 11:33:19 -0500

    add responsive layout link

--- storytime/js/java.js
+++ storytime/js/java.js
@@ -1,19 +1,9 @@
-var header = $('header');
-var range = 200;
-
-$(window).on('scroll', function () {
-  
-  var scrollTop = $(this).scrollTop(),
-      height = header.outerHeight(),
-      offset = height / 2,
-      calc = 1 - (scrollTop - offset + range) / range;
-
-  header.css({ 'opacity': calc });
-
-  if (calc > '1') {
-    header.css({ 'opacity': 1 });
-  } else if ( calc < '0' ) {
-    header.css({ 'opacity': 0 });
+$('div').on('inview', function(event, isInView) {
+  if (isInView) {
+    // element is now visible in the viewport
+  } else {
+    // element has gone out of viewport
   }
-  
 });
+
+$('div').off('inview');
