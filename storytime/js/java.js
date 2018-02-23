7aff360e97aa95a0928ee1a0af9130aa9391c60d
gordc262 <35770587+gordc262@users.noreply.github.com>
2018-02-23 09:22:39 -0500

    add documentation

--- storytime/js/java.js
+++
@@ -1,9 +0,0 @@
-$('div').on('inview', function(event, isInView) {
-  if (isInView) {
-    // element is now visible in the viewport
-  } else {
-    // element has gone out of viewport
-  }
-});
-
-$('div').off('inview');
