c5f6a15f28ba59d1d7be80101acc212d8a03391a
gordc262 <gordc262@newschool.edu>
2018-05-04 10:42:27 -0400

    wrktest

--- portfolio/app.js
+++ portfolio/app.js
@@ -1,10 +1,9 @@
-	var amplitude = 40; // hoogte vd golf beweging
-	var periode = Math.PI / 400; // lengte van 1 keer op en neer in rad
-	var evenwicht = 300; //start hoogte 
+	var amplitude = 40;
+	var periode = Math.PI / 400;
+	var evenwicht = 300;
 
 	$(document).ready(function(e) {
 
-	  //zet startHoogte meteen
 	  $(".block").css({
 	    "top": evenwicht
 	  });
@@ -14,8 +13,6 @@
 
 	  $(window).mousemove(function(evt) {
 
-	    //muisPosities
-	    //http://stackoverflow.com/questions/3234256/find-mouse-position-relative-to-element
 	    var mX = evt.pageX - $('.eyes').offset().left - $('.eyes').width() / 2;
 	    var mY = evt.pageY - $('.eyes').offset().top - $('.eyes').height() / 2;
 
@@ -90,3 +87,92 @@
 	);
 	});
 
+$('.slider').each(function() {
+  var $this = $(this);
+  var $group = $this.find('.slide_group');
+  var $slides = $this.find('.slide');
+  var bulletArray = [];
+  var currentIndex = 0;
+  var timeout;
+  
+  function move(newIndex) {
+    var animateLeft, slideLeft;
+    
+    advance();
+    
+    if ($group.is(':animated') || currentIndex === newIndex) {
+      return;
+    }
+    
+    bulletArray[currentIndex].removeClass('active');
+    bulletArray[newIndex].addClass('active');
+    
+    if (newIndex > currentIndex) {
+      slideLeft = '100%';
+      animateLeft = '-100%';
+    } else {
+      slideLeft = '-100%';
+      animateLeft = '100%';
+    }
+    
+    $slides.eq(newIndex).css({
+      display: 'block',
+      left: slideLeft
+    });
+    $group.animate({
+      left: animateLeft
+    }, function() {
+      $slides.eq(currentIndex).css({
+        display: 'none'
+      });
+      $slides.eq(newIndex).css({
+        left: 0
+      });
+      $group.css({
+        left: 0
+      });
+      currentIndex = newIndex;
+    });
+  }
+  
+  function advance() {
+    clearTimeout(timeout);
+    timeout = setTimeout(function() {
+      if (currentIndex < ($slides.length - 1)) {
+        move(currentIndex + 1);
+      } else {
+        move(0);
+      }
+    }, 4000);
+  }
+  
+  $('.next_btn').on('click', function() {
+    if (currentIndex < ($slides.length - 1)) {
+      move(currentIndex + 1);
+    } else {
+      move(0);
+    }
+  });
+  
+  $('.previous_btn').on('click', function() {
+    if (currentIndex !== 0) {
+      move(currentIndex - 1);
+    } else {
+      move(3);
+    }
+  });
+  
+  $.each($slides, function(index) {
+    var $button = $('<a class="slide_btn">&bull;</a>');
+    
+    if (index === currentIndex) {
+      $button.addClass('active');
+    }
+    $button.on('click', function() {
+      move(index);
+    }).appendTo('.slide_buttons');
+    bulletArray.push($button);
+  });
+  
+  advance();
+});
